d113267207a8dea177bbde824afce784470338ed
abhijith <abhijitht210@gmail.com>
2025-11-06 21:40:51 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blog1" element={<Blog1 />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
